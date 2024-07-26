57b0b9223e9a5961fce9ae42003e341cba55328a
Hetvee Kholiya <hetvee.kholiya@gmail.com>
2024-07-26 18:22:40 +0530

    do practice of loops

--- 01_Exercise.js
+++ 01_Exercise.js
@@ -2,7 +2,7 @@
 The program then takes input from the user to tell them whether the guess is correct, grater or lower 
 then orignal number.100(numberv of guess) is the score of the user the program is accepted to terminal 
 once the number is guess number should be between 1 - 100. */
-
+/*
 let random_Int = Math.random() * 100;
 random_Int = Math.round(random_Int);
 console.log(random_Int);
@@ -14,4 +14,38 @@ while (random_Int != value) {
     chance += 1
 }
 console.log("guess the correct value");
-console.log(` you took ${chance} chances the guess the right answer.`);
+console.log(` you took ${chance} chances the guess the right answer.`);*/
+
+let random_num = Math.random() * 100 ;
+random_num = Number.parseInt(random_num);
+console.log("randome number is : ", random_num);
+let chance = 1;
+let value = prompt("enter  number : ");
+while (random_num != value) {
+    console.log("Number is " , random_num > value ? "grater then " + value : "less then " + value  );
+   value = prompt ("guess number again : ");
+    chance += 1;
+}
+console.log(" well done ! you guess the correct value. ")
+console.log(` you take ${chance} to guess the correct value.`);
+
+/* // without using prompt
+let number = Math.random() * 10;
+number = Number.parseInt(number);
+console.log("randome number is : ", number);
+let varo  = 1;
+
+let value = console.log("enter any numbar :"+ value);
+
+if (number != value) {
+    console.log("number is not same");
+} else {
+    console.log(value > number ? "number is grater then " + value : "number is less then " + value );
+    value = console.log("enter again new number : ", value);
+    varo += 1;
+}
+console.log("well done !  you choose right number . ");
+console.log(`u take ${varo} to guess right value.`);
+
+*/
+
